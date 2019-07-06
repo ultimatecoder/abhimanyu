740c2a141cfd75e50eb557534e06913470bbb1af
ultimatecoder <jaysinhp@gmail.com>
2019-07-06 18:56:17 +0530

    Inprogress work of making 'Abhimanyu' as a working Python package

---
+++ web/setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name="abhimanyu",
+    packages=["abhimanyu"],
+    include_package_data=True,
+    install_requires=[
+        "flask",
+    ],
+)
